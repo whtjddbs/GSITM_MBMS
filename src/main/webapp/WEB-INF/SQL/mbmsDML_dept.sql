4088adc987808dd3fe1c86b958d4fdf50bd1e2e8
whtjddbs <blackrnfk@gmail.com>
2019-05-22 11:44:10 +0900

    0522 - sql update

---
+++ src/main/webapp/WEB-INF/SQL/mbmsDML_dept.sql
@@ -0,0 +1,13 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 화요일-5월-21-2019   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table DEPARTMENT
+Insert into MBMS.DEPARTMENT (DEPT_NO,DEPT_NAME,BOSS_NO,MGR_DEPT_NO) values (10,'SI사업부','IT1010',0);
+Insert into MBMS.DEPARTMENT (DEPT_NO,DEPT_NAME,BOSS_NO,MGR_DEPT_NO) values (11,'ICT사업팀','IT1011',10);
+Insert into MBMS.DEPARTMENT (DEPT_NO,DEPT_NAME,BOSS_NO,MGR_DEPT_NO) values (12,'SI사업팀','IT1012',10);
+Insert into MBMS.DEPARTMENT (DEPT_NO,DEPT_NAME,BOSS_NO,MGR_DEPT_NO) values (13,'Commerce파트','IT1013',12);
+Insert into MBMS.DEPARTMENT (DEPT_NO,DEPT_NAME,BOSS_NO,MGR_DEPT_NO) values (14,'CRM파트','IT1014',12);
+Insert into MBMS.DEPARTMENT (DEPT_NO,DEPT_NAME,BOSS_NO,MGR_DEPT_NO) values (15,'데이터사업파트','IT1015',12);
+Insert into MBMS.DEPARTMENT (DEPT_NO,DEPT_NAME,BOSS_NO,MGR_DEPT_NO) values (16,'유통/물류파트','IT1016',12);
+Insert into MBMS.DEPARTMENT (DEPT_NO,DEPT_NAME,BOSS_NO,MGR_DEPT_NO) values (0,'GSITM','CEO',0);
