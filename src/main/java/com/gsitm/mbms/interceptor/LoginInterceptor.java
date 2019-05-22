20effed239ee1b64e7c11270a768d0e76ce35ba4
whtjddbs <blackrnfk@gmail.com>
2019-05-22 11:41:11 +0900

    190522 - jsy first upload

--- src/main/java/com/gsitm/mbms/interceptor/LoginInterceptor.java
+++ src/main/java/com/gsitm/mbms/interceptor/LoginInterceptor.java
@@ -23,7 +23,7 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 	@Autowired
 	private EmployeeDAO EmployeeDAO;
 	
-	@Override
+	@Override 
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		HttpSession session = request.getSession();
