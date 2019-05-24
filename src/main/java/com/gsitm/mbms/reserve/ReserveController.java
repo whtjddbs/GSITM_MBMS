171005639fa5c8f98dd69fcfaf6b2dced925927a
whtjddbs <blackrnfk@gmail.com>
2019-05-24 13:51:11 +0900

    0524 - reserve start

---
+++ src/main/java/com/gsitm/mbms/reserve/ReserveController.java
@@ -0,0 +1,16 @@
+package com.gsitm.mbms.reserve;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @주제 : 
+ * @작성일 : 2019. 5. 24.
+ * @작성자 : 조성윤
+ */
+@Controller
+@RequestMapping("/reserve")
+public class ReserveController {
+
+	
+}
