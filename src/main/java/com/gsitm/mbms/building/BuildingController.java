f6204bfdd5ed6cb2f0f9dce67aec603781bf3cee
KimWonBIni <40154895+KimWonBIni@users.noreply.github.com>
2019-05-29 09:35:03 +0900

    BuildingList Complete
    빌딩리스트 DB가져와서 뿌리기 성공 >_<

--- src/main/java/com/gsitm/mbms/building/BuildingController.java
+++ src/main/java/com/gsitm/mbms/building/BuildingController.java
@@ -1,10 +1,16 @@
 package com.gsitm.mbms.building;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 
 /**
@@ -12,15 +18,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @작성자 : 김원빈
  */
 @Controller
+@RequestMapping("/building")
 public class BuildingController {
 
 	private static final Logger logger = LoggerFactory.getLogger(BuildingController.class);
 	
+	@Inject
+	private BuildingService service;
 	
 	//**로그인 화면*//*
-	@RequestMapping(value="/buildingSelectAll", method=RequestMethod.GET)
-	public String login() {
+	@RequestMapping(value="/buildingList", method=RequestMethod.GET)
+	public String list(Model model) {
+		
 		logger.info("Building List !!");
-		return "/building/buildingList";//test
+		
+		List<BuildingDTO> list = service.SelectAll();
+		System.out.println(list.size());
+		
+		model.addAttribute("list", list);
+		
+		return "building/buildingList";//test
 	}
+	
 }
---
+++ src/test/java/com/gsitm/mbms/BuildingDAOTest.java
@@ -0,0 +1,39 @@
+package com.gsitm.mbms;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.gsitm.mbms.building.BuildingDAO;
+import com.gsitm.mbms.building.BuildingDTO;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class BuildingDAOTest {
+   
+   @Inject
+   private BuildingDAO dao;
+   
+   Logger logger = Logger.getLogger(BuildingDAOTest.class);
+   
+   /** 승인 대기 목록 조회 테스트 */
+   @Test
+   public void TestWaitingList() {
+      
+      List<BuildingDTO> list = dao.SelectAll();
+      
+      for (BuildingDTO buildingDTO : list) {
+		logger.info(buildingDTO);
+	}
+      
+   }
+
+}
