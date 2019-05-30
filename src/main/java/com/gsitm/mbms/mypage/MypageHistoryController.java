cb4904415a94d33979ff667b227a8acc18cf9dc4
joohyun <joohyun27@gmail.com>
2019-05-30 09:30:45 +0900

    mypage history and mypage status

---
+++ src/main/java/com/gsitm/mbms/mypage/MypageHistoryController.java
@@ -0,0 +1,25 @@
+package com.gsitm.mbms.mypage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @작성일 : 2019. 5. 28.
+ * @작성자 : 이주현
+ */
+
+public class MypageHistoryController {
+	private static final Logger logger = LoggerFactory.getLogger(MypageHistoryController.class);
+
+	private MypageHistoryService service;
+
+	/** 예약내역 목록 */
+//	public String mypageHistoryList{
+//		
+//		@RequestMapping("/mypageHistoryList")
+//		return "mypage/mypageHistoryList";
+//	}
+	
+}
---
+++ src/main/java/com/gsitm/mbms/mypage/MypageHistoryDTO.java
@@ -0,0 +1,10 @@
+package com.gsitm.mbms.mypage;
+
+/**
+ * @작성일 : 2019. 5. 28.
+ * @작성자 : 이주현
+ */
+
+public class MypageHistoryDTO {
+
+}
