cd7a6eedbad8e20d2769e49fc02bc018cf6bb5cb
KimWonBIni <40154895+KimWonBIni@users.noreply.github.com>
2019-05-31 16:26:23 +0900

    우편번호, 삭제확인모달 완료

--- src/main/java/com/gsitm/mbms/building/BuildingController.java
+++ src/main/java/com/gsitm/mbms/building/BuildingController.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 /**
@@ -78,4 +77,6 @@ public class BuildingController {
 		service.buildingUpdate(dto);
 		return "redirect:/building/buildingList";
 	}
+	
+	
 }
